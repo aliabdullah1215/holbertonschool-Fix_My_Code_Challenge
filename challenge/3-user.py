555813dc34d6f73fbeb516190d6a4de48d7c326a
root <root@ip-10-42-185-139.ec2.internal>
2026-04-30 13:32:25 +0000

    Fix User class and password validation

--- challenge/3-user.py
+++ challenge/3-user.py
@@ -1,16 +1,37 @@
 #!/usr/bin/python3
+"""User class
+"""
 
-class User:
+
+class User():
+    """User class
+    """
+
     def __init__(self):
         self.__password = None
 
-    def set_password(self, pwd):
-        if pwd is None:
-            self.__password = None
-        else:
-            self.__password = pwd
+    @property
+    def password(self):
+        """Password getter
+        """
+        return self.__password
+
+    @password.setter
+    def password(self, pwd):
+        """Password setter
+        """
+        self.__password = pwd
 
     def is_valid_password(self, pwd):
-        if self.__password is None:
-            return False
+        """Valid password
+        """
         return self.__password == pwd
+
+
+if __name__ == "__main__":
+    print("Test User")
+
+    user_1 = User()
+    user_1.password = "myPassword"
+    if user_1.is_valid_password("myPassword") is False:
+        print("is_valid_password should return True if it's the right password")
